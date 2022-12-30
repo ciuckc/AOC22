27cf2e6dd906cc4b053589a0c8eea7ce854d6078
scristia <scristia@student.codam.nl>
2022-12-30 04:42:05 +0100

    small print mistake

--- day1/calorie_count.scala
+++ day1/calorie_count.scala
@@ -3,11 +3,11 @@ import scala.io.Source
 object main extends App {
 	val inventoryElfs = Source.fromFile("input.txt").mkString.split("\n\n")
 	val summedUp = Array.newBuilder[Int]
-	inventoryElfs.foreach { str =>
+	inventoryElfs.foreach{ str =>
 		val numbers = str.split("\n").map(_.toInt).foldLeft(0)(_ + _)
 		summedUp += numbers
 	}
 	val result = summedUp.result().sortWith(_ > _)
-	val firstTask = result.head.println
-	val secondTask = result.take(3).sum.println
+	val firstTask = result.head
+	val secondTask = result.take(3).sum
 }
